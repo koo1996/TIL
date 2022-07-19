77723bdcee98ec699123600da1a00e5aa078ea51
koo1996 <koodo1996@gmail.com>
2022-07-19 17:59:21 +0900

    22.07.19 실습

--- SWEA/001.SW2029.py
+++ SWEA/001.SW2029.py
@@ -1,13 +1,13 @@
 
 
-T = int(input())
-result=[]
-for test_case in range(1,T+1):
-    a,b = input().split()
+T = int(input())  
+result=[] # 리스트 생성
+for test_case in range(1,T+1):  #T번 반복
+    a,b = input().split() 
     a=int(a)
     b=int(b)
-    c = a // b 
-    d = a % b
-    result.append('#%d %d %d' %(test_case,c,d))
+    c = a // b  # 몫을 c에 저장
+    d = a % b   # 나머지를 d에 저장
+    result.append('#%d %d %d' %(test_case,c,d)) #리스트에 저장
 
-print(*result, sep='\n')
+print(*result, sep='\n') #줄바꿈해서 출력
--- SWEA/004.SW2070.py
+++ SWEA/004.SW2070.py
@@ -1,12 +1,12 @@
 T = int(input())
-res=[]
-for test_case in range(1,T+1):
+res=[] # 리스트 생성
+for test_case in range(1,T+1): #T번 반복
     a,b = input().split()
-    if int(a) < int(b):
-        res.append('#%d <' %test_case)
+    if int(a) < int(b): #크기비교
+        res.append('#%d <' %test_case)  #< , > , = 리스트에 저장
     elif int(a) > int(b):
         res.append('#%d >' %test_case)
     else:
         res.append('#%d =' %test_case)
 
-print(*res, sep='\n')
+print(*res, sep='\n') #줄바꿈해서 출력
