e714222d4d4acb35497f0bc58063d30114ea573c
koo1996 <koodo1996@gmail.com>
2022-07-20 15:55:30 +0900

    22.07.20 실습

---
+++ SWEA/005.SW1933.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for test in range(1,N+1):
+    if N % test == 0:
+        print(test, end=' ') 
---
+++ SWEA/006.SW2019.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for test in range(0,N+1):
+    result = 2 ** test
+    print(result, end=' ')
