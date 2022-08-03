8e313e28248d5550895ee0f578a9009bdc10aef6
koo1996 <koodo1996@gmail.com>
2022-08-03 09:48:06 +0900

    22.08.02

--- BOJ/BOJ11286.py
+++ BOJ/BOJ11286.py
@@ -1,7 +1,18 @@
-N = int(input())
-list_1 = []
-for i in range(N):
-    M = int(input())
-    list_1.append(M)
-list_1.pop(max(list_1)-1)
-print(list_1)
+import sys
+input = sys.stdin.readline
+
+import heapq
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    
+    i = int(input())
+
+    if i == 0 and len(heap)!=0:       
+        print(heapq.heappop(heap)[1]) 
+    elif i == 0 and len(heap)==0 :    
+        print(0)
+    else:
+         heapq.heappush(heap, (abs(i),i))
