653c197e745736dd36b1b32ab1be5f7781fdb55f
koo1996 <koodo1996@gmail.com>
2022-08-07 22:50:35 +0900

    22.08.07

--- BOJ/BOJ2511.py
+++ BOJ/BOJ2511.py
@@ -5,15 +5,18 @@ score_B = 0
 for i in range(len(A)):
     if A[i] < B[i]:
         score_B += 3
+        win = 'B'
     elif A[i] > B[i]:
         score_A += 3
+        win = 'A'
     else:
         score_A += 1
         score_B += 1
 print(score_A,score_B)
+
 if score_A > score_B:
     print('A')
 elif score_A < score_B:
     print('B')
 else:
-    print('D')
+    print('D' if A == B else win) 
--- BOJ/BOJ4673.py
+++ BOJ/BOJ4673.py
@@ -1,3 +1,20 @@
-N, M = map(int,input().split())
-matrix = [list(map(int,input().split())) for _ in range(N)]
-print(matrix)
+result1 = []
+
+for i in range(1,10000):
+
+    if i >= 1000: #4536
+        sum = i + (i // 1000) + ((i % 1000) // 100) + ((i % 100) // 10) + (i % 10)
+        result1.append(sum)
+    elif i >=100: #124
+        sum = i + (i // 100) + ((i % 100) // 10) + (i % 10)
+        result1.append(sum)
+    elif i >=10:
+        sum = i + (i // 10) + (i % 10)
+        result1.append(sum)
+    elif i >= 1:
+        sum = i + i
+        result1.append(sum)
+
+for j in range(1,10000):
+    if j not in result1:
+        print(j)
