535bd294498364633d07fa48f081506ec5aba5b1
koo1996 <koodo1996@gmail.com>
2022-08-07 16:44:33 +0900

    22.08.07

---
+++ SWEA/014.SW5431.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for test in range(1,T+1): # 테스트 케이스 설정
+    result = [] # 빈 리스트 생성
+    N, K = map(int,input().split()) 
+    submit = list(map(int,input().split()))
+    for i in range(1,N+1): # 1부터 N번까지 반복
+        if i not in submit: # submit 안에 i가 없으면 i를 result에 저장
+            result.append(i)
+    print(f'#{test}', *result) # 결과값 출력.
