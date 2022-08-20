0a27a66bcd4dd71b708281415ad937be612166ef
koo1996 <koodo1996@gmail.com>
2022-08-20 10:19:58 +0900

    22.08.20

--- BOJ/BOJ11652.py
+++ BOJ/BOJ11652.py
@@ -1,10 +1,10 @@
 N = int(input())
 dict = {}
 for i in range(N):
-    A = input()
+    A = int(input())
     if A not in dict:
         dict[A] = 1
     else:
         dict[A] += 1
-M = max(dict,key=dict.get)
-print(min(M))
+dict = sorted(dict.items(), key=lambda x: (-x[1], x[0]))
+print(dict[0][0])
