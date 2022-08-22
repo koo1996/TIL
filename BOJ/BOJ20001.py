21c58098d3728db986350e9d73383ba28b20ca26
koo1996 <koodo1996@gmail.com>
2022-08-22 16:43:55 +0900

    22.08.22

--- BOJ/BOJ20001.py
+++ BOJ/BOJ20001.py
@@ -1,3 +1,19 @@
-N = int(input())
+stack = []
 
-M
+while True:
+    N = input()
+    if N == '문제':
+        stack.append(1)
+    elif N == '고무오리':
+        if not stack:
+            stack.append(1)
+            stack.append(1)
+        else:
+            stack.pop()
+    elif N == '고무오리 디버깅 끝':
+        break
+
+if not stack:
+    print('고무오리야 사랑해')
+else:
+    print('힝구')
