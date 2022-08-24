e29f27407a7ce4479c77ea3a73eae9e2d8d94fee
koo1996 <koodo1996@gmail.com>
2022-08-24 23:28:15 +0900

    22.08.24

---
+++ BOJ/BOJ2864.py
@@ -0,0 +1,14 @@
+A,B = map(int,input().split())
+
+result = []
+
+sum = A + B
+result.append(sum)
+for i in range(len(str((A)))):
+    if A[i] == 5:
+        A[i] == 6
+        result.append(A + B)
+print(result)
+
+A = 12
+print((A))
