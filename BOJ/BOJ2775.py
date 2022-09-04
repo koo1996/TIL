1124c68249aae143ca2151a7d82636a76fde6978
koo1996 <koodo1996@gmail.com>
2022-09-04 16:04:54 +0900

    22.09.04

---
+++ BOJ/BOJ2775.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for _ in range(N):  
+    floor = int(input())  
+    num = int(input())  
+    f0 = [x for x in range(1, num+1)]  
+    for k in range(floor):  
+        for i in range(1, num): 
+            f0[i] += f0[i-1]  
+    print(f0[-1])  
