faf0fe5d16689185d295db7a2df2b5e8377380a2
koo1996 <koodo1996@gmail.com>
2022-09-17 17:34:23 +0900

    22.09.17

--- SWEA/D1_SW2056.py
+++ SWEA/D1_SW2056.py
@@ -1,12 +1,26 @@
-for test in range(1,int(input())+1):
-    date = input()
-    a = date[0:4]
-    b = date[4:6]
-    if 1 <= int(b) <= 12:
-        continue
-    else:
+t=int(input())
+
+for i in range(t):
+    print('#%s' %(i+1),end=' ')
+    date=input()
+    year=int(date[:4])
+    month=int(date[4:6])
+    dd=int(date[6:])
+    if month <1 or month>12:
         print(-1)
-        break
-    c = date[6:8]
-    if int(c)
-    print(a,b,c)
+        continue
+
+    if month in [1,3,5,7,8,10,12]:
+        if dd <1 or dd>31:
+            print(-1)
+            continue
+    if month ==2:
+        if dd<1 or dd>28:
+            print(-1)
+            continue
+
+    if month in [4,6,9,11]:
+        if dd <1 or dd>30:
+            print(-1)
+            continue
+    print("%04d/%02d/%02d" %(year,month,dd))
