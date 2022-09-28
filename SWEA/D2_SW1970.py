9310fd42ff310c6ffed368c6de1fc4feccc81f78
koo1996 <koodo1996@gmail.com>
2022-09-28 18:35:25 +0900

    22.09.28

--- SWEA/D2_SW1970.py
+++ SWEA/D2_SW1970.py
@@ -1,5 +1,11 @@
-N = int(input())
-
-for i in range(1,N+1):
-    M = int(input())
-    
+T = int(input())
+for j in range(1,T+1):
+    N = int(input())
+    list_ = [50000,10000,5000,1000,500,100,50,10]
+    result = []
+    for i in range(len(list_)):
+        res = N // list_[i]
+        result.append(res)
+        N -= res * list_[i]
+    print(f'#{j}')
+    print(*result)
