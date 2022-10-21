cd98bce8b66e6405d9bda9e2259f1b37f9076759
koo1996 <koodo1996@gmail.com>
2022-10-21 17:37:51 +0900

    add urls.py

---
+++ Django/22.10.05/urls.py
@@ -0,0 +1,13 @@
+# URL 설정을  app 단위로!
+from django.urls import path
+from . import views
+app_name = 'articles'
+
+urlpatterns = [
+    # http://localhost:8000/
+    path('', views.index, name='index'),
+    # path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/update/', views.update, name='update'),
+]
