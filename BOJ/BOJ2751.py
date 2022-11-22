83a3242ccb4b18cee6e989e9b35a23cab3c5a0f3
koo1996 <koodo1996@gmail.com>
2022-11-22 15:45:23 +0900

    update

--- BOJ/BOJ2751.py
+++ BOJ/BOJ2751.py
@@ -6,4 +6,8 @@ for i in range(n):
 for i in sorted(l):
     sys.stdout.write(str(i)+'\n')
 
-## sys.stdout 출력 버퍼, 출력 버퍼가 지정되어 있지 않으면 터미널 출력  (표준 출력)
+# sys.stdout.write 출력 버퍼, 출력 버퍼가 지정되어 있지 않으면 터미널 출력  (표준 출력)
+# import sys
+# sys.stdout.wirte("aaa")
+# sys.stdout.wirte("bbb")
+# "aaabbb"
